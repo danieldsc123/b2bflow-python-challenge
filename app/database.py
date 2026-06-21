4dd5fb48bdce586934bb2317783fffcb0bea2467
daniel da silva carvalho <contasdn00@gmail.com>
2026-06-21 00:20:01 -0300

    feat: add Supabase contact repository

--- app/database.py
+++ app/database.py
@@ -2,8 +2,23 @@ from supabase import create_client
 
 from app.config import get_env_var
 
+
 def create_supabase_client():
     supabase_url = get_env_var("SUPABASE_URL")
     supabase_key = get_env_var("SUPABASE_KEY")
 
     return create_client(supabase_url, supabase_key)
+
+
+def get_contacts():
+    supabase = create_supabase_client()
+
+    response = (
+        supabase
+        .table("contacts")
+        .select("nome_contato, telefone")
+        .limit(3)
+        .execute()
+    )
+
+    return response.data
